1d1824832115101f3159a0791a6547bcd18bd574
millerbrant <61174340+millerbrant@users.noreply.github.com>
2020-05-17 15:24:38 -0400

    Add files via upload

---
+++ Portfolio/resources/Queries.sql
@@ -0,0 +1,114 @@
+------------------------------------------------------------------------------------------------------------------------------
+--List the following details of each employee: 
+--employee number, last name, first name, gender, and salary.
+--------------------------------------------------------------------------------------------------------------------------------
+
+SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
+FROM employees e
+
+	LEFT JOIN salaries s ON
+	e.emp_no = s.emp_no	
+
+;------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------
+--List employees who were hired in 1986.
+------------------------------------------------------------------------------------------------------------------------------
+
+SELECT emp_no, last_name, first_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' and '1986-12-31'
+ORDER BY hire_date
+
+
+;------------------------------------------------------------------------
+-------------------------------------------------------------------------
+--List the manager of each department with the following information: 
+--department number, department name, the manager employee number, 
+--last name, first name, and start and end employment dates.
+------------------------------------------------------------------------------------------------------------------------------------------
+
+SELECT 	d.dept_no, d.dept_name, m.emp_no, 
+		e.last_name, e.first_name, m.from_date, m.to_date
+
+FROM departments d
+
+	LEFT JOIN dept_manager m ON
+	d.dept_no = m.dept_no
+	
+		LEFT JOIN employees e ON
+		m.emp_no = e.emp_no
+;------------------------------------------------------------------------	
+-------------------------------------------------------------------------
+--List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+-------------------------------------------------------------------------
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+
+	LEFT JOIN dept_emp x ON
+	e.emp_no = x.emp_no
+	
+		LEFT JOIN departments d ON
+		x.dept_no = d.dept_no
+;------------------------------------------------------------------------
+-------------------------------------------------------------------------
+--List all employees whose first name is "Hercules" and 
+--last names begin with "B."
+-------------------------------------------------------------------------
+
+SELECT *
+FROM EMPLOYEES
+WHERE	first_name = 'Hercules'
+AND		last_name LIKE 'B%'
+
+;------------------------------------------------------------------------
+---------------------------------------------------------------
+--List all employees in the Sales department, including their 
+--employee number, last name, first name, and department name.
+---------------------------------------------------------------
+
+SELECT d.emp_no, e.last_name, e.first_name, x.dept_name
+
+FROM dept_emp d
+
+	LEFT JOIN employees e ON
+	d.emp_no = e.emp_no
+
+		LEFT JOIN departments x ON
+		d.dept_no = x.dept_no
+
+WHERE d.dept_no = 'd007'
+
+;------------------------------------------------------------------------
+---------------------------------------------------------------
+--List all employees in the Sales and Development departments, 
+--including their employee number, last name, first name, 
+--and department name.
+---------------------------------------------------------------
+
+SELECT d.emp_no, e.last_name, e.first_name, x.dept_name
+
+FROM dept_emp d
+
+	LEFT JOIN employees e ON
+	d.emp_no = e.emp_no
+
+		LEFT JOIN departments x ON
+		d.dept_no = x.dept_no
+
+WHERE d.dept_no IN ('d007','d005')
+					
+;------------------------------------------------------------------------
+-----------------------------------------------------
+--In descending order, list the frequency count of 
+--employee last names, i.e., how many employees 
+--share each last name
+----------------------------------------------------
+
+SELECT last_name, COUNT(last_name) as name_count
+from employees 
+group by last_name
+order by name_count desc
+
+;------------------------------------------------------------------------
---
+++ Portfolio/resources/Schema.sql
@@ -0,0 +1,55 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE departments (
+    dept_no varchar(10) Primary Key NOT NULL,
+    dept_name varchar(50)   NOT NULL
+     )
+;
+CREATE TABLE dept_emp (
+    emp_no varchar(10)   NOT NULL,
+    dept_no varchar(10)   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
+     )
+;
+
+CREATE TABLE dept_manager (
+    dept_no varchar(10)   NOT NULL,
+    emp_no int primary key  NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
+     )
+;
+
+CREATE TABLE employees (
+    emp_no int   NOT NULL,
+    birth_date date   NOT NULL,
+    first_name varchar(50)   NOT NULL,
+    last_name varchar(50)   NOT NULL,
+    gender varchar(1)   NOT NULL,
+    hire_date date   NOT NULL
+)
+;
+
+CREATE TABLE salaries (
+    emp_no int   NOT NULL,
+    salary int   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
+)
+;
+
+CREATE TABLE titles (
+    emp_no int   NOT NULL,
+    title varchar(50)   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
+);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no)
